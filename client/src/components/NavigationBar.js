40b21578aeb069d8819ae26084ae3a40925fd877
ILRSBandara <160006775+ILRSBandara@users.noreply.github.com>
2024-12-11 02:37:08 +0530

    Created signIn

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -369,7 +369,22 @@ export default function NavigationBar() {
                 </div>
               </PopoverGroup>
 
-Z
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-gray-700 hover:text-gray-800"
+                  >
+                    Sign in
+                  </a>
+                  <span aria-hidden="true" className="h-6 w-px bg-gray-200" />
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-gray-700 hover:text-gray-800"
+                  >
+                    Create account
+                  </a>
+                </div>
 
                 <div className="hidden lg:ml-8 lg:flex">
                   <a
