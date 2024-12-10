871fef5cc2bbc68edaec1b1fd6e0ceaf44dc9954
ILRSBandara <160006775+ILRSBandara@users.noreply.github.com>
2024-12-11 02:32:54 +0530

    Created NavBar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client";
+
+import { Fragment, useState } from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@headlessui/react";
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  ShoppingBagIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+
+const navigation = {
+  
+};
 
 export default function NavigationBar() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>
-        <h1>NavigationBar</h1>
-    </div>
-  )
+    
+  );
 }
