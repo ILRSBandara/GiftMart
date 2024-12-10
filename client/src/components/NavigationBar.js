96e0b82cf5cf8d507cf2d7fb36df57f5c08402db
ILRSBandara <160006775+ILRSBandara@users.noreply.github.com>
2024-12-11 02:36:41 +0530

    Added LKR

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -369,7 +369,22 @@ export default function NavigationBar() {
                 </div>
               </PopoverGroup>
 
+Z
 
+                <div className="hidden lg:ml-8 lg:flex">
+                  <a
+                    href="#"
+                    className="flex items-center text-gray-700 hover:text-gray-800"
+                  >
+                    <img
+                      alt=""
+                      src="https://tailwindui.com/plus/img/flags/flag-canada.svg"
+                      className="block h-auto w-5 shrink-0"
+                    />
+                    <span className="ml-3 block text-sm font-medium">LKR</span>
+                    <span className="sr-only">, change currency</span>
+                  </a>
+                </div>
 
              
 
