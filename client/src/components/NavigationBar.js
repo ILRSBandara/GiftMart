5e5223f5de438bf333aa61123ac0957616278ffc
ILRSBandara <160006775+ILRSBandara@users.noreply.github.com>
2024-12-11 02:35:23 +0530

    Created Header

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -243,7 +243,37 @@ export default function NavigationBar() {
         </div>
       </Dialog>
 
-     
+      <header className="relative bg-white">
+        <nav
+          aria-label="Top"
+          className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"
+        >
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              <button
+                type="button"
+                onClick={() => setOpen(true)}
+                className="relative rounded-md bg-white p-2 text-gray-400 lg:hidden"
+              >
+                <span className="absolute -inset-0.5" />
+                <span className="sr-only">Open menu</span>
+                <Bars3Icon aria-hidden="true" className="size-6" />
+              </button>
+
+            
+
+            
+
+
+
+             
+
+               
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 }
